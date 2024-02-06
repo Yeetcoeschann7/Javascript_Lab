8c57c6969ddf52f1f15cc10799178d52f32626e3
Jacob L. <jlewis.cw743@gmail.com>
2024-02-05 22:54:37 -0500

    session 1 progress. Can't figure out how to link javascript correctly.

--- src/Index.py
+++ src/Index.py
@@ -1,7 +1,8 @@
 import tornado.web
+import os
 
 class Handler(tornado.web.RequestHandler):
     def get(self):
         self.write('Index')
-        #self.render("profilepage.html")
+        self.render("..\\html\\roulette.html")
 
