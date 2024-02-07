202e6f13008393ecd63834791d7fc6f39e21e3b3
Jacob L. <jlewis.cw743@gmail.com>
2024-02-07 18:22:48 -0500

    Clickable button spins wheel and prints outcome to console.

--- html/roulette.js
+++ html/roulette.js
@@ -1,40 +1,61 @@
 "use strict";
-console.log("Test Start\n");
-let wheel_arr = [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, -1, 15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36];
-let rand_i = Math.floor(Math.random() * 38); //Picks a random number between 0 and 38
-let final_print = "Random number picked: ";
 
-// Prints Number
-if(wheel_arr[rand_i] == -1){
-    final_print = final_print.concat("00, ");
-}
-else{
-    final_print = final_print.concat(String(wheel_arr[rand_i]));
-    final_print = final_print.concat(", ");
-}
+function main(){
+    console.log("Test Start\n");
+    let output_div = document.getElementById("output_div");
+    let spin_button = document.getElementById("spin_button");
+    let wheel_arr = [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, -1, 15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36];
 
-// Prints Color
-if(rand_i % 2 == 0){
-    final_print = final_print.concat("red, ");
-}
-else{
-    final_print = final_print.concat("black, ");
+    spin_button.addEventListener("mouseover", () => {
+        spin_button.style.backgroundColor="#808080";
+    });
+    spin_button.addEventListener("mouseout", () => {
+        spin_button.style.backgroundColor="#ebebeb"; //reset to default
+    });
+    spin_button.addEventListener("click", () => {
+        spin_wheel(wheel_arr);
+    });
 }
 
-// Prints Even/Odd
-if(wheel_arr[rand_i] % 2 == 0){
-    final_print = final_print.concat("even, ");
-}
-else{
-    final_print = final_print.concat("odd, ");
+function spin_wheel(wheel_arr){
+    let rand_i = Math.floor(Math.random() * 38); //Picks a random number between 0 and 38
+    let final_print = "Spin outcome: ";
+        
+    // Prints Number
+    if(wheel_arr[rand_i] == -1){
+        final_print = final_print.concat("00, ");
+    }
+    else{
+        final_print = final_print.concat(String(wheel_arr[rand_i]));
+        final_print = final_print.concat(", ");
+    }
+    
+    // Prints Color
+    if(rand_i % 2 == 0){
+        final_print = final_print.concat("red, ");
+    }
+    else{
+        final_print = final_print.concat("black, ");
+    }
+
+    // Prints Even/Odd
+    if(wheel_arr[rand_i] % 2 == 0){
+        final_print = final_print.concat("even, ");
+    }
+    else{
+        final_print = final_print.concat("odd, ");
+    }
+
+    // Prints Pass/Fail
+    if((wheel_arr[rand_i] >= 1 && wheel_arr[rand_i] <= 18) || wheel_arr[rand_i] == -1){
+        final_print = final_print.concat("passed.");
+    }
+    else{
+        final_print = final_print.concat("failed.");
+    }
+
+    console.log(final_print);
+    return final_print;
 }
 
-// Prints Pass/Fail
-if((wheel_arr[rand_i] >= 1 && wheel_arr[rand_i] <= 18) || wheel_arr[rand_i] == -1){
-    final_print = final_print.concat("passed.");
-}
-else{
-    final_print = final_print.concat("failed.");
-}
-
-console.log(final_print);
+main()
